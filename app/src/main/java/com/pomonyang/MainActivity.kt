0fafc10fc4e933c4bc6a810e9c9ee923507b40df
ezhoon <dlwlgns1240@gmail.com>
2024-07-19 19:48:30 +0900

    feature: NetworkErrorScreen Sample 추가

--- app/src/main/java/com/pomonyang/MainActivity.kt
+++ app/src/main/java/com/pomonyang/MainActivity.kt
@@ -4,30 +4,65 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.pomonyang.data.remote.util.NetworkConnectivityManager
 import com.pomonyang.ui.theme.PomonyangTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var networkConnectivityManager: NetworkConnectivityManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PomonyangTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                    val isConnected by networkConnectivityManager.connectionFlow.collectAsStateWithLifecycle(
+                        networkConnectivityManager.isConnected
                     )
+
+                    Box(
+                        modifier =
+                            Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        if (isConnected) {
+                            Greeting(name = "Android")
+                        } else {
+                            NetworkErrorScreen()
+                        }
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun NetworkErrorScreen(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier,
+        text = "Network Error",
+        color = Color.Red
+    )
 }
 
 @Composable
@@ -36,8 +71,8 @@ fun Greeting(
     modifier: Modifier = Modifier
 ) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        text = "Hello $name!"
     )
 }
 
@@ -46,5 +81,13 @@ fun Greeting(
 fun GreetingPreview() {
     PomonyangTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NetworkErrorScreenPreview() {
+    PomonyangTheme {
+        NetworkErrorScreen()
     }
 }
