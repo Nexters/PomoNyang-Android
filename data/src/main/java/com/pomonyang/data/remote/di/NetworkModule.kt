4aa42a93490e40fcaaad3bd5041e4a8b7b4029b6
ezhoon <53300830+lee-ji-hoon@users.noreply.github.com>
2024-07-19 23:25:15 +0900

    [CAT-32] 네트워크 상태를 갖고 있는 NetworkConnectivityManager 구현 (#15)
    * feature: ACCESS_NETWORK_STATE 권한 추가
    
    * feature: NetworkConnectivityManager 구현
    
    * feature: NetworkErrorScreen Sample 추가
    
    * feature: ktlint 정렬 옵션 수정

--- data/src/main/java/com/pomonyang/data/remote/di/NetworkModule.kt
+++ data/src/main/java/com/pomonyang/data/remote/di/NetworkModule.kt
@@ -4,7 +4,10 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import com.pomonyang.data.BuildConfig
 import com.pomonyang.data.remote.interceptor.HttpRequestInterceptor
 import com.pomonyang.data.remote.service.PomoNyangService
+import com.pomonyang.data.remote.util.NetworkConnectivityManager
+import com.pomonyang.data.remote.util.NetworkConnectivityManagerImpl
 import com.pomonyang.data.remote.util.NetworkResultCallAdapterFactory
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,51 +20,57 @@ import retrofit2.Retrofit
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object NetworkModule {
-    private const val BASE_URL = BuildConfig.BASE_URL
-
-    @Provides
+internal abstract class NetworkModule {
+    @Binds
     @Singleton
-    fun provideJsonBuilder(): Json =
-        Json {
-            ignoreUnknownKeys = true // dto 정의되어 있지 않은 필드도 허락
-            coerceInputValues = true // 해당 타입의 기본값으로 세팅되게
-            isLenient = true // String이지만 Int로 들어올 때 등등 자동으로 파싱
-            if (BuildConfig.DEBUG) prettyPrint = true
-        }
+    abstract fun bindNetworkConnectivityManager(networkConnectivityManagerImpl: NetworkConnectivityManagerImpl): NetworkConnectivityManager
 
-    @Provides
-    @Singleton
-    fun provideHTTPRequestInterceptor(): HttpRequestInterceptor = HttpRequestInterceptor()
+    companion object {
+        private const val BASE_URL = BuildConfig.BASE_URL
 
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(httpRequestInterceptor: HttpRequestInterceptor): OkHttpClient =
-        OkHttpClient
-            .Builder()
-            .addInterceptor(httpRequestInterceptor)
-            .build()
+        @Provides
+        @Singleton
+        fun provideJsonBuilder(): Json =
+            Json {
+                ignoreUnknownKeys = true // dto 정의되어 있지 않은 필드도 허락
+                coerceInputValues = true // 해당 타입의 기본값으로 세팅되게
+                isLenient = true // String이지만 Int로 들어올 때 등등 자동으로 파싱
+                if (BuildConfig.DEBUG) prettyPrint = true
+            }
 
-    @Provides
-    @Singleton
-    fun provideNetworkResultCallAdapter(): NetworkResultCallAdapterFactory = NetworkResultCallAdapterFactory()
+        @Provides
+        @Singleton
+        fun provideHTTPRequestInterceptor(): HttpRequestInterceptor = HttpRequestInterceptor()
 
-    @Provides
-    @Singleton
-    fun provideRetrofit(
-        okHttpClient: OkHttpClient,
-        networkResultCallAdapterFactory: NetworkResultCallAdapterFactory,
-        json: Json
-    ): Retrofit =
-        Retrofit
-            .Builder()
-            .client(okHttpClient)
-            .baseUrl(BASE_URL)
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-            .addCallAdapterFactory(networkResultCallAdapterFactory)
-            .build()
+        @Provides
+        @Singleton
+        fun provideOkHttpClient(httpRequestInterceptor: HttpRequestInterceptor): OkHttpClient =
+            OkHttpClient
+                .Builder()
+                .addInterceptor(httpRequestInterceptor)
+                .build()
 
-    @Provides
-    @Singleton
-    fun provideApiService(retrofit: Retrofit): PomoNyangService = retrofit.create(PomoNyangService::class.java)
+        @Provides
+        @Singleton
+        fun provideNetworkResultCallAdapter(): NetworkResultCallAdapterFactory = NetworkResultCallAdapterFactory()
+
+        @Provides
+        @Singleton
+        fun provideRetrofit(
+            okHttpClient: OkHttpClient,
+            networkResultCallAdapterFactory: NetworkResultCallAdapterFactory,
+            json: Json
+        ): Retrofit =
+            Retrofit
+                .Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL)
+                .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+                .addCallAdapterFactory(networkResultCallAdapterFactory)
+                .build()
+
+        @Provides
+        @Singleton
+        fun provideApiService(retrofit: Retrofit): PomoNyangService = retrofit.create(PomoNyangService::class.java)
+    }
 }
