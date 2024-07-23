58301ab82ef75065020710bd9974f179303f2194
Hyomk <qet557@gmail.com>
2024-07-23 22:54:13 +0900

    feat: httpRequestInterceptor 헤더 token 추가

--- data/src/main/java/com/pomonyang/data/remote/interceptor/HttpRequestInterceptor.kt
+++ data/src/main/java/com/pomonyang/data/remote/interceptor/HttpRequestInterceptor.kt
@@ -1,16 +1,25 @@
 package com.pomonyang.data.remote.interceptor
 
+import com.pomonyang.data.local.datastore.datasource.token.TokenDataSource
+import javax.inject.Inject
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 
-internal class HttpRequestInterceptor : Interceptor {
+internal class HttpRequestInterceptor @Inject constructor(
+    private val tokenDataSource: TokenDataSource
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
+        val accessToken = runBlocking {
+            tokenDataSource.getAccessToken()
+        }
         val origin = chain.request()
         val request =
             origin
                 .newBuilder()
                 .addHeader("Content-Type", "application/x-www-form-urlencoded")
                 .addHeader("Accept", "*/*")
+                .addHeader("Authorization", "Bearer $accessToken")
                 .build()
         return chain.proceed(request)
     }
