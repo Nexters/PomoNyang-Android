7a089fef7d7299d7c9702145d71ed391209d1a49
Hyomk <qet557@gmail.com>
2024-07-24 23:24:37 +0900

    feat: AuthDataStore Singleton annotation 추가

--- data/src/main/java/com/pomonyang/data/remote/di/RemoteDataSourceModule.kt
+++ data/src/main/java/com/pomonyang/data/remote/di/RemoteDataSourceModule.kt
@@ -6,10 +6,12 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class RemoteDataSourceModule {
     @Binds
+    @Singleton
     abstract fun provideAuthDataSource(authDataSourceImpl: AuthDataSourceImpl): AuthDataSource
 }
