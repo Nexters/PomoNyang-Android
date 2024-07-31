9e13152561d822cfcb0f48b7c511f290908feeaf
Hyomk <qet557@gmail.com>
2024-08-01 02:05:35 +0900

    feat: 사이즈별 아이콘 컴포넌트 구현

---
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/icon/MNXLargeIcon.kt
@@ -0,0 +1,27 @@
+package com.pomonyang.mohanyang.presentation.designsystem.icon
+
+import IconSize
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import com.mohanyang.presentation.R
+
+@Composable
+fun MNXLargeIcon(
+    modifier: Modifier = Modifier,
+    @DrawableRes resourceId: Int = R.drawable.ic_null,
+    contentDescription: String? = null,
+    tint: Color = LocalContentColor.current
+) {
+    Icon(
+        painter = painterResource(id = resourceId),
+        contentDescription = contentDescription,
+        modifier = modifier.size(IconSize.xLarge),
+        tint = tint
+    )
+}
