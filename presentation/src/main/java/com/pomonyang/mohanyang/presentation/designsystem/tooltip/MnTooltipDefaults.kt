5772d73276a5022f82e9e16de40c92c09678c62a
ezhoon <53300830+lee-ji-hoon@users.noreply.github.com>
2024-08-03 12:47:36 +0900

    [CAT-122] MnTooltip 구현 (#41)

---
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/tooltip/MnTooltipDefaults.kt
@@ -0,0 +1,38 @@
+package com.pomonyang.mohanyang.presentation.designsystem.tooltip
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.pomonyang.mohanyang.presentation.designsystem.token.MnColor
+import com.pomonyang.mohanyang.presentation.theme.MnTheme
+
+object MnTooltipDefaults {
+    val anchorWidth = 14.dp
+    val anchorHeight = 9.dp
+    val overlayBackgroundColor = MnColor.Black.copy(alpha = 0.5f)
+
+    @Composable
+    fun lightTooltipColors(
+        containerColor: Color = MnColor.White,
+        contentColor: Color = MnTheme.textColorScheme.secondary
+    ) = MnTooltipColors(
+        containerColor = containerColor,
+        contentColor = contentColor
+    )
+
+    @Composable
+    fun darkTooltipColors(
+        containerColor: Color = MnTheme.backgroundColorScheme.inverse,
+        contentColor: Color = MnTheme.iconColorScheme.inverse
+    ) = MnTooltipColors(
+        containerColor = containerColor,
+        contentColor = contentColor
+    )
+}
+
+@Immutable
+data class MnTooltipColors(
+    val containerColor: Color,
+    val contentColor: Color
+)
