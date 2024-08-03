a71b6f0999ef4c4de620bcdac35505f02c0d5b07
ezhoon <dlwlgns1240@gmail.com>
2024-08-03 13:57:52 +0900

    feat: 온보딩 스크린에 Dialog 임시 구현

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/onboarding/OnboardingScreen.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/onboarding/OnboardingScreen.kt
@@ -5,8 +5,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.pomonyang.mohanyang.presentation.designsystem.dialog.MnDialog
 import com.pomonyang.mohanyang.presentation.util.DevicePreviews
 import com.pomonyang.mohanyang.presentation.util.ThemePreviews
 
@@ -27,11 +32,27 @@ private fun OnboardingScreen(
     modifier: Modifier = Modifier,
     onHomeClick: () -> Unit
 ) {
+    var isShowing by remember { mutableStateOf(false) }
+
+    if (isShowing) {
+        MnDialog(
+            title = "Dialog Title",
+            subTitle = "Home으로 이동하시겠어요?",
+            positiveButtonLabel = "이동",
+            negativeButtonLabel = "취소",
+            onPositiveButtonClick = {
+                isShowing = false
+                onHomeClick()
+            },
+            onNegativeButtonClick = { isShowing = false },
+            onDismissRequest = { isShowing = false }
+        )
+    }
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Button(onHomeClick) {
+        Button({ isShowing = true }) {
             Text(text = "go to home")
         }
     }
