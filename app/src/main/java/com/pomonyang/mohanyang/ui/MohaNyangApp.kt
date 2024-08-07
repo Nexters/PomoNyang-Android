64d16c66fc23df6d1d9f544cf87e09b2dfc06376
ezhoon <dlwlgns1240@gmail.com>
2024-08-07 23:45:47 +0900

    feat: App단에서 windowInset 동작하지 않게 수정

--- app/src/main/java/com/pomonyang/mohanyang/ui/MohaNyangApp.kt
+++ app/src/main/java/com/pomonyang/mohanyang/ui/MohaNyangApp.kt
@@ -1,5 +1,6 @@
 package com.pomonyang.mohanyang.ui
 
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration.Short
@@ -34,7 +35,8 @@ private fun MohaNyangApp(
 ) {
     Scaffold(
         modifier = modifier,
-        snackbarHost = { SnackbarHost(snackbarHostState) }
+        snackbarHost = { SnackbarHost(snackbarHostState) },
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
     ) { innerPadding ->
 
         val isOffline by mohaNyangAppState.isOffline.collectAsStateWithLifecycle()
