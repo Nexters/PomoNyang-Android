e01903f6a0e2fdf8a9ed119669c583c8c66723d2
Hyomk <qet557@gmail.com>
2024-08-07 10:11:55 +0900

    feat: onboarding navigation 추가

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/onboarding/OnboardingNavigator.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/onboarding/OnboardingNavigator.kt
@@ -1,20 +1,60 @@
 package com.pomonyang.mohanyang.presentation.screen.onboarding
 
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.pomonyang.mohanyang.presentation.screen.home.Home
+import com.pomonyang.mohanyang.presentation.screen.onboarding.guide.OnboardingGuideRoute
+import com.pomonyang.mohanyang.presentation.screen.onboarding.naming.OnboardingNamingCatRoute
+import com.pomonyang.mohanyang.presentation.screen.onboarding.select.OnboardingSelectCatRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
 data object Onboarding
 
+@Serializable
+internal data object OnboardingGuide
+
+@Serializable
+internal data object OnboardingSelectCat
+
+@Serializable
+internal data object OnboardingNamingCat
+
 fun NavGraphBuilder.onboardingScreen(
     navigateUp: () -> Unit,
-    onHomeClick: () -> Unit
+    navHostController: NavHostController
 ) {
-    composable<Onboarding> {
-        OnboardingRoute(
-            onNavigateUp = navigateUp,
-            onHomeClick = onHomeClick
-        )
+    navigation<Onboarding>(
+        startDestination = OnboardingGuide
+    ) {
+        composable<OnboardingGuide> {
+            OnboardingGuideRoute(
+                onStartClick = {
+                    navHostController.navigate(OnboardingSelectCat)
+                }
+            )
+        }
+
+        composable<OnboardingSelectCat> {
+            OnboardingSelectCatRoute(
+                onNamingClick = {
+                    navHostController.navigate(OnboardingNamingCat)
+                }
+            )
+        }
+
+        composable<OnboardingNamingCat> {
+            OnboardingNamingCatRoute(
+                onHomeClick = {
+                    navHostController.navigate(
+                        route = Home,
+                        navOptions = NavOptions.Builder().setPopUpTo<OnboardingGuide>(false).build()
+                    )
+                }
+            )
+        }
     }
 }
