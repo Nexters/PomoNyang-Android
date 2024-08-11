6ed4fab5b7fb1b4310817b5ebf8ae9b0ca45805b
ezhoon <dlwlgns1240@gmail.com>
2024-08-11 20:04:53 +0900

    refactor: 필요없는 내용 제거

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/util/FlowUtils.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/util/FlowUtils.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 inline fun <reified T> Flow<T>.collectWithLifecycle(
@@ -18,20 +17,6 @@ inline fun <reified T> Flow<T>.collectWithLifecycle(
     LaunchedEffect(this, lifecycleOwner) {
         lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
             this@collectWithLifecycle.collect { action(it) }
-        }
-    }
-}
-
-@Composable
-inline fun <reified T> Flow<T>.collectLatestWithLifecycle(
-    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
-    noinline action: suspend (T) -> Unit
-) {
-    val lifecycleOwner = LocalLifecycleOwner.current
-
-    LaunchedEffect(this, lifecycleOwner) {
-        lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
-            this@collectLatestWithLifecycle.collectLatest { action(it) }
         }
     }
 }
