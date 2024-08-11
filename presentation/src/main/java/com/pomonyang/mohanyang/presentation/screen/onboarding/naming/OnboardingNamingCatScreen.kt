9bdab9e193c6e398efcb6694146c52f4f4858041
Hyomk <qet557@gmail.com>
2024-08-11 11:02:01 +0900

    feat: 고양이 이름짓기 화면 디자인 변경안 반영

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/onboarding/naming/OnboardingNamingCatScreen.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/onboarding/naming/OnboardingNamingCatScreen.kt
@@ -1,11 +1,17 @@
 package com.pomonyang.mohanyang.presentation.screen.onboarding.naming
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,11 +19,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -31,6 +39,7 @@ import com.pomonyang.mohanyang.presentation.designsystem.button.icon.MnIconButto
 import com.pomonyang.mohanyang.presentation.designsystem.textfield.MnTextField
 import com.pomonyang.mohanyang.presentation.designsystem.token.MnColor
 import com.pomonyang.mohanyang.presentation.designsystem.token.MnSpacing
+import com.pomonyang.mohanyang.presentation.designsystem.tooltip.tooltip
 import com.pomonyang.mohanyang.presentation.designsystem.topappbar.MnTopAppBar
 import com.pomonyang.mohanyang.presentation.theme.MnTheme
 import com.pomonyang.mohanyang.presentation.util.noRippleClickable
@@ -78,14 +87,17 @@ fun OnboardingNamingCatScreen(
     val focusManager = LocalFocusManager.current
     val keyboardController = LocalSoftwareKeyboardController.current
 
+    val listState = rememberLazyListState()
+
     Column(
         modifier = modifier
             .fillMaxSize()
-            .imePadding()
             .noRippleClickable {
                 focusManager.clearFocus(true)
                 keyboardController?.hide()
             }
+            .imePadding()
+
     ) {
         MnTopAppBar(navigationIcon = {
             MnIconButton(
@@ -94,38 +106,67 @@ fun OnboardingNamingCatScreen(
             )
         })
 
-        Column(
+        LazyColumn(
+            state = listState,
             modifier = Modifier
-                .padding(horizontal = MnSpacing.xLarge)
-                .padding(bottom = MnSpacing.small)
+                .padding(
+                    horizontal = MnSpacing.xLarge
+                )
         ) {
-            Text(
-                modifier = Modifier.padding(
-                    top = MnSpacing.twoXLarge,
-                    bottom = MnSpacing.medium
-                ),
-                text = stringResource(R.string.naming_cat),
-                style = MnTheme.typography.header5,
-                color = MnTheme.textColorScheme.primary
-            )
-            MnTextField(
-                modifier = Modifier.fillMaxWidth(),
-                value = name,
-                isError = state.isError && name.isNotEmpty(),
-                errorMessage = state.errorMessage,
-                backgroundColor = MnColor.White,
-                onValueChange = { value -> name = value }
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            MnBoxButton(
-                text = stringResource(R.string.complete),
-                onClick = { onAction(NamingEvent.OnComplete(name)) },
-                isEnabled = name.isNotEmpty(),
-                modifier = Modifier.fillMaxWidth(),
-                colors = MnBoxButtonColorType.primary,
-                styles = MnBoxButtonStyles.large
-            )
+            item {
+                CatRive(modifier = Modifier.padding(top = 130.dp))
+                Text(
+                    modifier = Modifier.padding(
+                        top = MnSpacing.twoXLarge,
+                        bottom = MnSpacing.small
+                    ),
+                    text = stringResource(R.string.naming_cat),
+                    style = MnTheme.typography.subBodyRegular,
+                    color = MnTheme.textColorScheme.secondary
+                )
+                MnTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = name,
+                    isError = state.isError && name.isNotEmpty(),
+                    errorMessage = state.errorMessage,
+                    backgroundColor = MnColor.White,
+                    onValueChange = { value -> name = value }
+                )
+            }
         }
+        Spacer(modifier = Modifier.weight(1f))
+        MnBoxButton(
+            text = stringResource(R.string.complete),
+            onClick = { onAction(NamingEvent.OnComplete(name)) },
+            containerPadding = PaddingValues(
+                start = MnSpacing.xLarge,
+                end = MnSpacing.xLarge,
+                bottom = MnSpacing.small
+            ),
+            isEnabled = name.isNotEmpty(),
+            modifier = Modifier.fillMaxWidth(),
+            colors = MnBoxButtonColorType.primary,
+            styles = MnBoxButtonStyles.large
+        )
+    }
+}
+
+@Composable
+fun CatRive(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(240.dp)
+            .background(MnTheme.backgroundColorScheme.secondary)
+            .tooltip(
+                stringResource(id = R.string.naming_cat_tooltip),
+                showOverlay = false,
+                enabled = false // TODO 툴팁 적용시 포커스 불가문제 해결
+
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "image")
     }
 }
 
