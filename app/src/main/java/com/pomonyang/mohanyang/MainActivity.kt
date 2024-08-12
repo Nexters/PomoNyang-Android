0f22c0690e1739818fa3bbe1545882b85b9599eb
Hyomk <qet557@gmail.com>
2024-08-12 23:58:42 +0900

    feat: 신규유저 오프라인 상태 block dialog 추가

--- app/src/main/java/com/pomonyang/mohanyang/MainActivity.kt
+++ app/src/main/java/com/pomonyang/mohanyang/MainActivity.kt
@@ -6,18 +6,29 @@ import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.pomonyang.mohanyang.data.remote.util.NetworkMonitor
 import com.pomonyang.mohanyang.data.repository.pomodoro.PomodoroSettingRepository
 import com.pomonyang.mohanyang.data.repository.user.UserRepository
 import com.pomonyang.mohanyang.domain.usecase.GetTokenByDeviceIdUseCase
+import com.pomonyang.mohanyang.presentation.designsystem.button.box.MnBoxButton
+import com.pomonyang.mohanyang.presentation.designsystem.button.box.MnBoxButtonColorType
+import com.pomonyang.mohanyang.presentation.designsystem.button.box.MnBoxButtonStyles
+import com.pomonyang.mohanyang.presentation.designsystem.dialog.MnDialog
 import com.pomonyang.mohanyang.presentation.theme.MnTheme
 import com.pomonyang.mohanyang.ui.MohaNyangApp
 import com.pomonyang.mohanyang.ui.rememberMohaNyangAppState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -40,20 +51,45 @@ class MainActivity : ComponentActivity() {
         handleSplashScreen()
         enableEdgeToEdge()
         setContent {
+            val coroutineScope = rememberCoroutineScope()
+
+            val isNewUser = userRepository.isNewUser()
+            var showDialog by remember { mutableStateOf(isNewUser && !networkMonitor.isConnected) }
+
+            fun getUserTokenByDeviceId() {
+                coroutineScope.launch {
+                    showDialog = isNewUser && getTokenByDeviceIdUseCase().isFailure
+                }
+            }
             LaunchedEffect(Unit) {
-                getTokenByDeviceIdUseCase()
+                getUserTokenByDeviceId()
                 pomodoroSettingRepository.fetchPomodoroSettingList()
             }
 
             MnTheme {
-                val coroutineScope = rememberCoroutineScope()
                 val mohaNyangAppState = rememberMohaNyangAppState(
                     isNewUser = userRepository.isNewUser(),
                     networkMonitor = networkMonitor,
                     coroutineScope = coroutineScope
                 )
-
-                MohaNyangApp(mohaNyangAppState = mohaNyangAppState)
+                if (showDialog) {
+                    MnDialog(
+                        title = "네트워크 연결 실패",
+                        subTitle = "네트워크 연결에 실패하였습니다.\n확인 후 다시 시도해 주세요",
+                        positiveButton = {
+                            MnBoxButton(
+                                modifier = Modifier.fillMaxWidth(),
+                                text = "새로고침",
+                                onClick = { getUserTokenByDeviceId() },
+                                colors = MnBoxButtonColorType.primary,
+                                styles = MnBoxButtonStyles.medium
+                            )
+                        },
+                        onDismissRequest = {}
+                    )
+                } else {
+                    MohaNyangApp(mohaNyangAppState = mohaNyangAppState)
+                }
             }
         }
     }
--- domain/src/main/java/com/pomonyang/mohanyang/domain/usecase/GetTokenByDeviceIdUseCase.kt
+++ domain/src/main/java/com/pomonyang/mohanyang/domain/usecase/GetTokenByDeviceIdUseCase.kt
@@ -1,20 +1,19 @@
 package com.pomonyang.mohanyang.domain.usecase
 
+import com.pomonyang.mohanyang.data.remote.model.response.TokenResponse
 import com.pomonyang.mohanyang.data.repository.user.UserRepository
 import javax.inject.Inject
 
 class GetTokenByDeviceIdUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke() {
+    suspend operator fun invoke(): Result<TokenResponse> {
         val deviceId = userRepository.getDeviceId()
-        if (deviceId.isNotEmpty()) {
-            userRepository.login(deviceId).onSuccess {
-                userRepository.saveToken(
-                    accessToken = it.accessToken,
-                    refreshToken = it.refreshToken
-                )
-            }.onFailure { throw it }
+        return userRepository.login(deviceId).onSuccess {
+            userRepository.saveToken(
+                accessToken = it.accessToken,
+                refreshToken = it.refreshToken
+            )
         }
     }
 }
