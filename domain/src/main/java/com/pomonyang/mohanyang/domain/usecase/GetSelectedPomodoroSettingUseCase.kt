b3e3bbc9360c6df5aeb1a10bb4f95606d3730a01
ezhoon <dlwlgns1240@gmail.com>
2024-08-16 22:29:43 +0900

    feat: PomodoroCategoryType 필드 추가 및 적용

--- domain/src/main/java/com/pomonyang/mohanyang/domain/usecase/GetSelectedPomodoroSettingUseCase.kt
+++ domain/src/main/java/com/pomonyang/mohanyang/domain/usecase/GetSelectedPomodoroSettingUseCase.kt
@@ -1,20 +1,17 @@
 package com.pomonyang.mohanyang.domain.usecase
 
+import com.pomonyang.mohanyang.data.local.room.enitity.PomodoroSettingEntity
 import com.pomonyang.mohanyang.data.repository.pomodoro.PomodoroSettingRepository
-import com.pomonyang.mohanyang.domain.model.setting.PomodoroCategoryModel
-import com.pomonyang.mohanyang.domain.model.setting.toModel
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.zip
 
 class GetSelectedPomodoroSettingUseCase @Inject constructor(
     private val pomodoroSettingRepository: PomodoroSettingRepository
 ) {
 
-    operator fun invoke(): Flow<PomodoroCategoryModel> {
+    operator fun invoke(): Flow<PomodoroSettingEntity> {
         val settingListFlow = pomodoroSettingRepository.getPomodoroSettingList()
-            .map { list -> list.map { it.toModel() } }
         val recentUseCategoryNoFlow = pomodoroSettingRepository.getRecentUseCategoryNo()
 
         return settingListFlow.zip(recentUseCategoryNoFlow) { settingList, recentUseCategoryNo ->
