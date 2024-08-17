9e5a2131a20fb1c563597a7c56daa7b686b2117f
김효민 <78139690+HyomK@users.noreply.github.com>
2024-08-18 01:40:59 +0900

    [CAT-237] MnNotificationManager 구현 (#76)

---
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/util/MnNotificationManager.kt
@@ -0,0 +1,38 @@
+package com.pomonyang.mohanyang.presentation.util
+
+import android.content.Context
+import android.content.Intent
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
+object MnNotificationManager {
+
+    const val INTENT_NOTIFY_FOCUS_MESSAGE = "mohanyang.intent.NOTIFY_FOCUS_MESSAGE"
+    const val INTENT_NOTIFY_REST_MESSAGE = "mohanyang.intent.NOTIFY_REST_MESSAGE"
+    const val INTENT_START_INTERRUPT_MESSAGE = "mohanyang.intent.START_INTERRUPT_MESSAGE"
+    const val INTENT_STOP_INTERRUPT_MESSAGE = "mohanyang.intent.STOP_INTERRUPT_MESSAGE"
+    const val INTENT_SEND_MESSAGE = "mohanyang.intent.SEND_MESSAGE"
+
+    val intents = listOf(
+        INTENT_NOTIFY_FOCUS_MESSAGE,
+        INTENT_NOTIFY_REST_MESSAGE,
+        INTENT_START_INTERRUPT_MESSAGE,
+        INTENT_STOP_INTERRUPT_MESSAGE,
+        INTENT_SEND_MESSAGE
+    )
+
+    fun notifyTimerEnd(context: Context) {
+        LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(INTENT_NOTIFY_FOCUS_MESSAGE))
+    }
+
+    fun notifyRestEnd(context: Context) {
+        LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(INTENT_NOTIFY_REST_MESSAGE))
+    }
+
+    fun startInterrupt(context: Context) {
+        LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(INTENT_START_INTERRUPT_MESSAGE))
+    }
+
+    fun stopInterrupt(context: Context) {
+        LocalBroadcastManager.getInstance(context).sendBroadcast(Intent(INTENT_STOP_INTERRUPT_MESSAGE))
+    }
+}
