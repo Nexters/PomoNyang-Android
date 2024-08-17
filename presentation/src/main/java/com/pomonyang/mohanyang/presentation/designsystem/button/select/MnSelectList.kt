233438622ff9a27377c1acf44076a9684f9b5412
Hyomk <qet557@gmail.com>
2024-08-17 19:48:25 +0900

    feat: 화면 아이콘 교체

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/button/select/MnSelectList.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/button/select/MnSelectList.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -49,7 +50,7 @@ fun MnSelectListItem(
             ) {
                 MnMediumIcon(
                     resourceId = iconResource,
-                    tint = MnTheme.iconColorScheme.primary
+                    tint = Color.Unspecified
                 )
 
                 Text(
