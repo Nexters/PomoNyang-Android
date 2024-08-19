e4039e349ebcb66825dca65b8321d2844ea81a5b
ezhoon <dlwlgns1240@gmail.com>
2024-08-19 11:09:29 +0900

    feat: text width 고정

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/component/Timer.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/component/Timer.kt
@@ -3,15 +3,22 @@ package com.pomonyang.mohanyang.presentation.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import com.mohanyang.presentation.R
+import com.pomonyang.mohanyang.presentation.designsystem.token.MnSpacing
 import com.pomonyang.mohanyang.presentation.screen.PomodoroConstants.DEFAULT_TIME
 import com.pomonyang.mohanyang.presentation.theme.MnTheme
 
@@ -21,24 +28,54 @@ fun Timer(
     exceededTime: String,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(
-            modifier = Modifier.width(136.dp),
-            text = time,
-            style = MnTheme.typography.header1,
-            color = MnTheme.textColorScheme.primary
-        )
-        if (exceededTime != DEFAULT_TIME) {
-            Row(modifier = Modifier.width(95.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            time.toCharArray().forEach { num ->
                 Text(
-                    modifier = Modifier,
-                    text = exceededTime,
-                    style = MnTheme.typography.header4,
-                    color = MnTheme.backgroundColorScheme.accent1
+                    modifier = Modifier
+                        .width(if (num != ':') 31.5.dp else 16.dp)
+                        .offset(y = if (num == ':') (-4).dp else 0.dp),
+                    text = "$num",
+                    style = MnTheme.typography.header1.copy(
+                        platformStyle = PlatformTextStyle(
+                            includeFontPadding = false
+                        )
+                    ),
+                    color = MnTheme.textColorScheme.primary,
+                    textAlign = TextAlign.Center
                 )
+            }
+        }
 
+        if (exceededTime != DEFAULT_TIME) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                exceededTime.toCharArray().forEach { num ->
+                    Text(
+                        modifier = Modifier
+                            .width(if (num != ':') 13.dp else 6.dp)
+                            .offset(y = if (num == ':') (-2).dp else 0.dp),
+                        text = "$num",
+                        style = MnTheme.typography.header4.copy(
+                            platformStyle = PlatformTextStyle(
+                                includeFontPadding = false
+                            )
+                        ),
+                        color = MnTheme.backgroundColorScheme.accent1,
+                        textAlign = TextAlign.Center
+                    )
+                }
                 Text(
-                    text = "초과",
+                    modifier = Modifier.padding(start = MnSpacing.xSmall),
+                    text = stringResource(R.string.timer_exceed_time),
                     style = MnTheme.typography.header4,
                     color = MnTheme.backgroundColorScheme.accent1
                 )
@@ -50,12 +87,12 @@ fun Timer(
 private class TimerPreviewParameterProvider : PreviewParameterProvider<TimerPreviewParams> {
     override val values = sequenceOf(
         TimerPreviewParams(
-            time = "25:00",
+            time = "30:00",
             exceededTime = DEFAULT_TIME
         ),
         TimerPreviewParams(
             time = "00:00",
-            exceededTime = "05:00"
+            exceededTime = "00:01"
         ),
         TimerPreviewParams(
             time = "10:00",
