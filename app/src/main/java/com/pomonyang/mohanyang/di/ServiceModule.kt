fd2558be6e2e690770515888d41c79be5ddeaaaf
ezhoon <dlwlgns1240@gmail.com>
2024-09-09 22:23:35 +0900

    fix: component 수정

--- app/src/main/java/com/pomonyang/mohanyang/di/ServiceModule.kt
+++ app/src/main/java/com/pomonyang/mohanyang/di/ServiceModule.kt
@@ -11,11 +11,12 @@ import com.pomonyang.mohanyang.ui.ServiceHelper
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
 
 @Module
-@InstallIn(ServiceModule::class)
+@InstallIn(ServiceComponent::class)
 internal object ServiceModule {
 
     @Provides
