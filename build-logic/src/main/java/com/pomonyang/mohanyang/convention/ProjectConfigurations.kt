28cc962a9ab938d8e555289060746fecbf4076a3
ezhoon <53300830+lee-ji-hoon@users.noreply.github.com>
2024-09-11 07:00:28 +0900

    chore: 라이브러리 업데이트 및 build-logic 일부 정리 (#111)

--- build-logic/src/main/java/com/pomonyang/mohanyang/convention/ProjectConfigurations.kt
+++ build-logic/src/main/java/com/pomonyang/mohanyang/convention/ProjectConfigurations.kt
@@ -6,5 +6,5 @@ object ProjectConfigurations {
     const val COMPILE_SDK = 34
     const val MIN_SDK = 26
     const val TARGET_SDK = 34
-    val JAVA_VERSION = JavaVersion.VERSION_17
+    val JAVA_VERSION = JavaVersion.VERSION_21
 }
