5f5574445812a7cd3a6f1e49e08a44dfc892c9ed
ezhoon <53300830+lee-ji-hoon@users.noreply.github.com>
2024-09-11 23:02:09 +0900

    HttpRequest-UserAgent 구현 (#110)

--- data/src/main/java/com/pomonyang/mohanyang/data/remote/interceptor/HttpRequestInterceptor.kt
+++ data/src/main/java/com/pomonyang/mohanyang/data/remote/interceptor/HttpRequestInterceptor.kt
@@ -1,13 +1,20 @@
 package com.pomonyang.mohanyang.data.remote.interceptor
 
+import android.content.Context
+import android.os.Build
+import com.mohanyang.data.BuildConfig
+import com.pomonyang.mohanyang.data.local.datastore.datasource.deviceid.DeviceIdLocalDataSource
 import com.pomonyang.mohanyang.data.local.datastore.datasource.token.TokenLocalDataSource
+import java.util.Locale
 import javax.inject.Inject
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
 
 internal class HttpRequestInterceptor @Inject constructor(
-    private val tokenLocalDataSource: TokenLocalDataSource
+    private val tokenLocalDataSource: TokenLocalDataSource,
+    private val deviceIdLocalDataSource: DeviceIdLocalDataSource,
+    private val context: Context
 ) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val accessToken = runBlocking {
@@ -21,7 +28,35 @@ internal class HttpRequestInterceptor @Inject constructor(
                 .addHeader("Accept", "*/*")
                 .removeHeader("Authorization")
                 .header("Authorization", "Bearer $accessToken")
+                .header("User-Agent", getUserAgent())
                 .build()
         return chain.proceed(request)
+    }
+
+    private fun getUserAgent(): String {
+        val packageInfo = context.packageManager?.getPackageInfo(context.packageName, 0)
+        val version = packageInfo?.versionName ?: ""
+
+        val finalVersion = if (version.split("\\.").size > 3) {
+            version.substring(0, version.lastIndexOf("."))
+        } else {
+            version
+        }
+
+        val deviceId = runBlocking {
+            deviceIdLocalDataSource.getDeviceId()
+        }
+
+        return String.format(
+            Locale.KOREA,
+            "%s/%s/%s (Android %s, %s, %s, %s)",
+            BuildConfig.BUILD_TYPE,
+            finalVersion,
+            deviceId,
+            Build.VERSION.RELEASE,
+            Build.MODEL,
+            Build.BRAND,
+            Locale.getDefault().language
+        )
     }
 }
