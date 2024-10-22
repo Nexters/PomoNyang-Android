c22315f0766451d3e7a66471cbf58eadfc67db9e
Hyomk <qet557@gmail.com>
2024-10-22 12:46:13 +0900

    refactor: MainActivity refactoring

--- app/src/main/java/com/pomonyang/mohanyang/MainActivity.kt
+++ app/src/main/java/com/pomonyang/mohanyang/MainActivity.kt
@@ -8,28 +8,32 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.startup.AppInitializer
 import app.rive.runtime.kotlin.RiveInitializer
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import com.pomonyang.mohanyang.data.remote.util.NetworkMonitor
-import com.pomonyang.mohanyang.data.repository.pomodoro.PomodoroSettingRepository
-import com.pomonyang.mohanyang.data.repository.pomodoro.PomodoroTimerRepository
-import com.pomonyang.mohanyang.data.repository.push.PushAlarmRepository
-import com.pomonyang.mohanyang.data.repository.user.UserRepository
-import com.pomonyang.mohanyang.domain.usecase.GetTokenByDeviceIdUseCase
 import com.pomonyang.mohanyang.notification.LocalNotificationReceiver
 import com.pomonyang.mohanyang.notification.util.createNotificationChannel
 import com.pomonyang.mohanyang.presentation.designsystem.button.box.MnBoxButton
@@ -38,7 +42,9 @@ import com.pomonyang.mohanyang.presentation.designsystem.button.box.MnBoxButtonS
 import com.pomonyang.mohanyang.presentation.designsystem.dialog.MnDialog
 import com.pomonyang.mohanyang.presentation.theme.MnTheme
 import com.pomonyang.mohanyang.presentation.util.MnNotificationManager
+import com.pomonyang.mohanyang.presentation.util.collectWithLifecycle
 import com.pomonyang.mohanyang.ui.MohaNyangApp
+import com.pomonyang.mohanyang.ui.MohaNyangAppState
 import com.pomonyang.mohanyang.ui.rememberMohaNyangAppState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -46,111 +52,143 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withTimeout
-import timber.log.Timber
 
 @OptIn(DelicateCoroutinesApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     @Inject
     lateinit var networkMonitor: NetworkMonitor
-
-    @Inject
-    lateinit var userRepository: UserRepository
-
-    @Inject
-    lateinit var pomodoroSettingRepository: PomodoroSettingRepository
-
-    @Inject
-    lateinit var pushAlarmRepository: PushAlarmRepository
-
-    @Inject
-    lateinit var getTokenByDeviceIdUseCase: GetTokenByDeviceIdUseCase
-
-    @Inject
-    lateinit var pomodoroTimerRepository: PomodoroTimerRepository
 
     private var keepSplashOnScreen = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         handleSplashScreen()
         super.onCreate(savedInstanceState)
-        AppInitializer.getInstance(applicationContext)
-            .initializeComponent(RiveInitializer::class.java)
-        GlobalScope.launch { pomodoroTimerRepository.savePomodoroCacheData() }
-        createNotificationChannel()
-        registerNotificationService()
-        initializeFCM()
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
-            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
-        )
+
+        configureAppStartup()
+
         setContent {
-            val coroutineScope = rememberCoroutineScope()
             val activity = (LocalContext.current as? Activity)
 
-            var isNewUser = userRepository.isNewUser()
-            var showDialog by remember { mutableStateOf(isNewUser && !networkMonitor.isConnected) }
-            suspend fun initializeInfo() {
-                getTokenByDeviceIdUseCase()
-                    .onSuccess {
-                        userRepository.fetchMyInfo().onSuccess {
-                            isNewUser = it.cat.no == -1
-                        }
-                        pomodoroSettingRepository.fetchPomodoroSettingList()
-                        showDialog = false
-                    }.onFailure {
-                        showDialog = isNewUser
-                    }
+            val state by viewModel.state.collectAsStateWithLifecycle()
+            val coroutineScope = rememberCoroutineScope()
+
+            val mohaNyangAppState = rememberMohaNyangAppState(
+                isNewUser = viewModel.checkIfNewUser(),
+                networkMonitor = networkMonitor,
+                coroutineScope = coroutineScope
+            )
+
+            var showDialog by remember { mutableStateOf(false) }
+
+            LaunchedEffect(Unit) {
+                viewModel.handleEvent(MainEvent.Init)
             }
 
-            runBlocking {
-                try {
-                    withTimeout(SPLASH_DELAY) {
-                        initializeInfo()
+            viewModel.effects.collectWithLifecycle { effect ->
+                when (effect) {
+                    MainEffect.ShowBlockDialog -> {
+                        showDialog = true
                     }
-                } catch (ignored: Exception) {
+
+                    MainEffect.DismissBlockDialog -> {
+                        showDialog = false
+                    }
+
+                    MainEffect.ExitApp -> {
+                        activity?.finish()
+                    }
+
+                    MainEffect.GoToTimer -> {}
+
+                    MainEffect.GoToOnBoarding -> {}
+
+
                 }
             }
 
             MnTheme {
-                val mohaNyangAppState = rememberMohaNyangAppState(
-                    isNewUser = isNewUser,
-                    networkMonitor = networkMonitor,
-                    coroutineScope = coroutineScope
+                if (showDialog) NewUserBlockDialog(
+                    onClickRefresh = {
+                        viewModel.handleEvent(MainEvent.ClickRefresh)
+                    }, onDismissRequest = {
+                        viewModel.handleEvent(MainEvent.ClickClose)
+                    }
                 )
-                if (showDialog) {
-                    MnDialog(
-                        properties = DialogProperties(
-                            dismissOnClickOutside = false,
-                            dismissOnBackPress = true
-                        ),
-                        title = "네트워크 연결 실패",
-                        subTitle = "네트워크 연결에 실패하였습니다.\n확인 후 다시 시도해 주세요",
-                        positiveButton = {
-                            MnBoxButton(
-                                modifier = Modifier.fillMaxWidth(),
-                                text = "새로고침",
-                                onClick = {
-                                    coroutineScope.launch {
-                                        initializeInfo()
-                                    }
-                                },
-                                colors = MnBoxButtonColorType.primary,
-                                styles = MnBoxButtonStyles.medium
-                            )
-                        },
-                        onDismissRequest = {
-                            activity?.finish()
-                        }
-                    )
-                } else {
-                    MohaNyangApp(mohaNyangAppState = mohaNyangAppState)
-                }
+                else AppScreen(viewState = state, mohaNyangAppState = mohaNyangAppState)
+            }
+        }
+
+    }
+
+    @Composable
+    fun AppScreen(viewState: MainState, mohaNyangAppState: MohaNyangAppState) {
+        when {
+            viewState.isLoading -> LoadingScreen()
+            viewState.isError -> ErrorScreen()
+            else -> {
+                MohaNyangApp(mohaNyangAppState = mohaNyangAppState)
             }
         }
     }
+
+    @Composable
+    fun LoadingScreen() {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator(modifier = Modifier.size(20.dp))
+        }
+    }
+
+    @Composable
+    fun ErrorScreen() {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text("에러발생!")
+        }
+    }
+
+    @Composable
+    fun NewUserBlockDialog(
+        onClickRefresh: () -> Unit,
+        onDismissRequest: () -> Unit,
+    ) {
+        MnDialog(
+            properties = DialogProperties(
+                dismissOnClickOutside = false,
+                dismissOnBackPress = true
+            ),
+            title = getString(R.string.dialog_network_title),
+            subTitle = getString(R.string.dialog_network_content),
+            positiveButton = {
+                MnBoxButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = getString(R.string.dialog_network_refresh),
+                    onClick = onClickRefresh,
+                    colors = MnBoxButtonColorType.primary,
+                    styles = MnBoxButtonStyles.medium,
+
+                    )
+            },
+            onDismissRequest = onDismissRequest
+        )
+    }
+
+
+    private fun configureAppStartup() {
+        AppInitializer.getInstance(applicationContext)
+            .initializeComponent(RiveInitializer::class.java)
+
+        createNotificationChannel()
+        registerNotificationService()
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
+    }
+
 
     private fun handleSplashScreen() {
         installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
@@ -166,22 +204,10 @@ class MainActivity : ComponentActivity() {
     }
 
     override fun onDestroy() {
-        GlobalScope.launch { pomodoroTimerRepository.updateRecentPomodoroDone() }
+        GlobalScope.launch { viewModel.updateRecentPomodoroDoneData() }
         super.onDestroy()
     }
 
-    private fun initializeFCM() {
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(
-            OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Timber.w("Fetching FCM registration token failed", task.exception)
-                    return@OnCompleteListener
-                }
-                val token = task.result
-                Timber.d("FCM : $token")
-            }
-        )
-    }
 
     private fun registerNotificationService() {
         val intentFilters = IntentFilter().apply {
@@ -190,6 +216,7 @@ class MainActivity : ComponentActivity() {
         val receiver = LocalNotificationReceiver()
         LocalBroadcastManager.getInstance(this).registerReceiver(receiver, intentFilters)
     }
+
 
     companion object {
         private const val SPLASH_DELAY = 2000L
---
+++ app/src/main/java/com/pomonyang/mohanyang/MainViewModel.kt
@@ -0,0 +1,205 @@
+package com.pomonyang.mohanyang
+
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.pomonyang.mohanyang.data.remote.model.response.UserInfoResponse
+import com.pomonyang.mohanyang.data.remote.util.NetworkMonitor
+import com.pomonyang.mohanyang.data.repository.pomodoro.PomodoroSettingRepository
+import com.pomonyang.mohanyang.data.repository.pomodoro.PomodoroTimerRepository
+import com.pomonyang.mohanyang.data.repository.push.PushAlarmRepository
+import com.pomonyang.mohanyang.data.repository.user.UserRepository
+import com.pomonyang.mohanyang.domain.usecase.GetTokenByDeviceIdUseCase
+import com.pomonyang.mohanyang.presentation.base.BaseViewModel
+import com.pomonyang.mohanyang.presentation.base.ViewEvent
+import com.pomonyang.mohanyang.presentation.base.ViewSideEffect
+import com.pomonyang.mohanyang.presentation.base.ViewState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
+
+
+data class MainState(
+    val isError: Boolean = false,
+    val isLoading: Boolean = true,
+) : ViewState
+
+sealed interface MainEvent : ViewEvent {
+    data object Init : MainEvent
+    data object ClickRefresh : MainEvent
+    data object ClickClose : MainEvent
+}
+
+sealed interface MainEffect : ViewSideEffect {
+    data object ShowBlockDialog : MainEffect
+    data object DismissBlockDialog : MainEffect
+    data object GoToOnBoarding : MainEffect
+    data object GoToTimer : MainEffect
+    data object ExitApp : MainEffect
+}
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val pomodoroTimerRepository: PomodoroTimerRepository,
+    private val getTokenByDeviceIdUseCase: GetTokenByDeviceIdUseCase,
+    private val pomodoroSettingRepository: PomodoroSettingRepository,
+    private val userRepository: UserRepository,
+    private val pushAlarmRepository: PushAlarmRepository,
+    private val networkMonitor: NetworkMonitor
+) : BaseViewModel<MainState, MainEvent, MainEffect>() {
+
+
+    override fun setInitialState(): MainState {
+        return MainState()
+    }
+
+    override fun handleEvent(event: MainEvent) {
+        when (event) {
+            MainEvent.Init -> {
+                initializeAppData()
+            }
+
+            MainEvent.ClickRefresh -> {
+                onClickRefresh()
+            }
+
+            MainEvent.ClickClose -> {
+                setEffect(MainEffect.ExitApp)
+            }
+        }
+    }
+
+
+    private fun initializeAppData() {
+        viewModelScope.launch {
+            try {
+                withTimeout(FETCH_MAX_TIME) {
+                    if (networkMonitor.isConnected) {
+                        onOnline()
+                    } else {
+                        onOffline()
+                    }
+                    updateState { copy(isLoading = false) }
+                }
+            } catch (e: TimeoutCancellationException) {
+                /* MAX TIME 초과시  Cancel 처리된 경우 네트워크 알림처리 */
+                updateState { copy(isLoading = false) }
+                setEffect(MainEffect.ShowBlockDialog)
+            } catch (e: Exception) {
+                Timber.w("${e.message}")
+                updateState { copy(isError = true, isLoading = false) }
+            }
+
+        }
+    }
+
+
+    // 온라인 상태일 때 실행할 초기화 로직
+    private fun onOnline() {
+        viewModelScope.launch {
+            fetchFcmToken()
+            fetchUserInfo()
+                .onSuccess {
+                    savePomodoroCacheData()
+                }
+        }
+    }
+
+    //오프라인 상태일 때 실행할 초기화 로직
+    private fun onOffline() {
+        viewModelScope.launch {
+            val isNewUser = checkIfNewUser()
+            Timber.d("offline : ${isNewUser}")
+            if (isNewUser) {
+                setEffect(MainEffect.ShowBlockDialog)
+            } else {
+                setEffect(MainEffect.GoToTimer)
+            }
+        }
+    }
+
+    fun checkIfNewUser() = userRepository.isNewUser()
+
+    private suspend fun fetchUserInfo(): Result<UserInfoResponse> {
+        return runCatching {
+            getTokenByDeviceIdUseCase().getOrThrow()
+            val userInfo = userRepository.fetchMyInfo().getOrThrow()
+            pomodoroSettingRepository.fetchPomodoroSettingList()
+            userInfo
+        }.onSuccess {
+            if (it.isNewUser()) {
+                setEffect(MainEffect.GoToOnBoarding)
+            } else {
+                setEffect(MainEffect.GoToTimer)
+            }
+        }.onFailure {
+            updateState { copy(isError = true) }
+        }
+    }
+
+    private fun onClickRefresh() {
+        if (!networkMonitor.isConnected) return
+
+        viewModelScope.launch {
+            updateState { copy(isLoading = true) }
+
+            fetchUserInfo()
+                .onSuccess {
+                    setEffect(MainEffect.DismissBlockDialog)
+                }
+
+            updateState { copy(isLoading = false) }
+        }
+    }
+
+
+    private fun fetchFcmToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Timber.w("Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+                val token = task.result
+                Timber.d("FCM : $token")
+                updateFcmToken(token)
+            }
+        )
+    }
+
+    private fun updateFcmToken(token: String) {
+        viewModelScope.launch {
+            pushAlarmRepository.saveFcmToken(token)
+            userRepository.getMyInfo().let {
+                if (it.isPushEnabled) {
+                    pushAlarmRepository.apply {
+                        registerPushToken(token)
+                    }
+                }
+            }
+        }
+    }
+
+
+    private suspend fun savePomodoroCacheData() {
+        pomodoroTimerRepository.savePomodoroCacheData()
+    }
+
+    suspend fun updateRecentPomodoroDoneData() {
+        pomodoroTimerRepository.updateRecentPomodoroDone()
+    }
+
+    private fun UserInfoResponse.isNewUser(): Boolean {
+        /* 서버에서 fetch 한 기본 고양이 id가 -1 인 경우 신규 유저로 판단 */
+        return this.cat.no == NEW_USER_VALIDATION_ID
+    }
+
+    companion object {
+        const val NEW_USER_VALIDATION_ID = -1
+        const val FETCH_MAX_TIME = 3000L
+    }
+
+}
