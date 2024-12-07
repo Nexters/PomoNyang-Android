33de839b21ffb718b2feaf841ccbcde049e2ac72
김효민 <78139690+HyomK@users.noreply.github.com>
2024-12-07 16:22:36 +0900

    [SCRUM-72] 마이페이지 통계 버튼 소거 및 문구 변경 (#155)

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/mypage/MyPageScreen.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/mypage/MyPageScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -319,16 +320,10 @@ fun CheckFocusTimeBox(
                 Text(
                     text = stringResource(id = R.string.my_page_static_subtitle),
                     style = MnTheme.typography.subBodyRegular,
-                    color = MnTheme.textColorScheme.secondary
+                    color = MnTheme.textColorScheme.secondary,
+                    textAlign = TextAlign.Center
                 )
             }
-
-            MnBoxButton(
-                text = stringResource(id = R.string.my_page_static_check),
-                onClick = { onAction.invoke(MyPageEvent.ClickStatic) },
-                colors = MnBoxButtonColorType.secondary,
-                styles = MnBoxButtonStyles.small
-            )
         }
     }
 }
