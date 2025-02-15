c7a8fd90e24f1b1ad9e1ec0d48fafd503d5515f9
lee-ji-hoon <dlwlgns1240@gmail.com>
2025-02-16 02:17:36 +0900

    refactor: MnSelectList 디자인 시스템 변경된 부분 반영
    https://www.figma.com/design/2HuhQQqKTYg2vWpRnvPKDI/%EB%BD%80%EB%AA%A8%EB%83%A5-%EB%94%94%EC%9E%90%EC%9D%B8%EC%8B%9C%EC%8A%A4%ED%85%9C?node-id=509-18221&t=OfXFwmzyxliUd7m2-4

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/button/select/MnSelectList.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/button/select/MnSelectList.kt
@@ -1,28 +1,21 @@
 package com.pomonyang.mohanyang.presentation.designsystem.button.select
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mohanyang.presentation.R
 import com.pomonyang.mohanyang.presentation.designsystem.icon.MnMediumIcon
-import com.pomonyang.mohanyang.presentation.designsystem.token.MnRadius
 import com.pomonyang.mohanyang.presentation.designsystem.token.MnSpacing
 import com.pomonyang.mohanyang.presentation.theme.MnTheme
 
@@ -30,8 +23,6 @@ import com.pomonyang.mohanyang.presentation.theme.MnTheme
 fun MnSelectListItem(
     iconResource: Int,
     categoryType: String,
-    focusTime: String,
-    restTime: String,
     modifier: Modifier = Modifier,
     containerPadding: PaddingValues = PaddingValues(),
     isSelected: Boolean = false,
@@ -62,55 +53,10 @@ fun MnSelectListItem(
                     style = MnTheme.typography.bodySemiBold,
                     color = MnTheme.textColorScheme.primary,
                 )
-                TimeComponent(
-                    type = stringResource(R.string.focus),
-                    time = focusTime,
-                )
-                TimeDivider()
-                TimeComponent(
-                    type = stringResource(R.string.rest),
-                    time = restTime,
-                )
             }
         },
         onClick = onClick,
     )
-}
-
-@Composable
-private fun TimeDivider() {
-    Box(
-        Modifier
-            .padding(horizontal = 4.dp)
-            .width(1.dp)
-            .height(12.dp)
-            .background(
-                color = MnTheme.iconColorScheme.tertiary,
-                shape = RoundedCornerShape(MnRadius.xSmall),
-            ),
-    )
-}
-
-@Composable
-private fun TimeComponent(
-    type: String,
-    time: String,
-    modifier: Modifier = Modifier,
-) {
-    Row(modifier) {
-        Text(
-            text = type,
-            style = MnTheme.typography.subBodyRegular,
-            color = MnTheme.textColorScheme.tertiary,
-        )
-
-        Text(
-            modifier = Modifier.padding(start = 4.dp),
-            text = time,
-            style = MnTheme.typography.subBodyRegular,
-            color = MnTheme.textColorScheme.tertiary,
-        )
-    }
 }
 
 @Preview
@@ -125,32 +71,24 @@ fun MnSelectListPreview(modifier: Modifier = Modifier) {
             categoryType = "기본",
             onClick = {},
             isSelected = true,
-            restTime = "25",
-            focusTime = "25",
             modifier = Modifier.fillMaxWidth(),
         )
         MnSelectListItem(
             iconResource = R.drawable.ic_null,
             categoryType = "공부",
             onClick = {},
-            restTime = "25",
-            focusTime = "25",
             modifier = Modifier.fillMaxWidth(),
         )
         MnSelectListItem(
             iconResource = R.drawable.ic_null,
             categoryType = "작업",
             onClick = {},
-            restTime = "25",
-            focusTime = "25",
             modifier = Modifier.fillMaxWidth(),
         )
         MnSelectListItem(
             iconResource = R.drawable.ic_null,
             categoryType = "운동",
             onClick = {},
-            restTime = "25",
-            focusTime = "25",
             modifier = Modifier.fillMaxWidth(),
         )
     }
