24e96bfe1079666d7e0b4cb204b953bae2030205
Hyomk <qet557@gmail.com>
2025-02-25 23:28:47 +0900

    feat: CategoryIcon 클래스 및 리소스 추가

---
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/screen/home/category/CategoryIcon.kt
@@ -0,0 +1,18 @@
+package com.pomonyang.mohanyang.presentation.screen.home.category
+
+import com.mohanyang.presentation.R
+
+enum class CategoryIcon(val id: String, val resourceId: Int) {
+    DEFAULT(id = "default", resourceId = R.drawable.ic_category_default),
+    MEMO(id = "memo", resourceId = R.drawable.ic_memo),
+    BOOK(id = "book", resourceId = R.drawable.ic_book),
+    BRIFECASE(id = "brifecase", resourceId = R.drawable.ic_brifecase),
+    LAPTOP(id = "laptop", resourceId = R.drawable.ic_laptop),
+    EXERCISE(id = "exercise", resourceId = R.drawable.ic_exercise),
+    REST(id = "rest", resourceId = R.drawable.ic_rest),
+    FOCUS(id = "focus", resourceId = R.drawable.ic_focus),
+    HEART(id = "heart", resourceId = R.drawable.ic_heart),
+    STAR(id = "star", resourceId = R.drawable.ic_star),
+    SUN(id = "sun", resourceId = R.drawable.ic_sun),
+    MOON(id = "moon", resourceId = R.drawable.ic_moon),
+}
