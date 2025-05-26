d1f856096571e8b712b236bb511924b9518b114b
Hyomk <qet557@gmail.com>
2025-05-26 18:52:38 +0900

    chore: radius 디자인시스템 업데이트

--- presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/token/MnRadius.kt
+++ presentation/src/main/java/com/pomonyang/mohanyang/presentation/designsystem/token/MnRadius.kt
@@ -3,6 +3,8 @@ package com.pomonyang.mohanyang.presentation.designsystem.token
 import androidx.compose.ui.unit.dp
 
 object MnRadius {
+    val twoXSmall = 4.dp
+    val threeXSmall = 8.dp
     val xSmall = 12.dp
     val small = 16.dp
     val medium = 20.dp
